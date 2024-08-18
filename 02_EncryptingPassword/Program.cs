2cf1371cc2b227ef2a91544c6d1cdd2d18a91605
bebinaflasica <miljan@pwn.rs>
2024-08-18 11:23:26 +0200

    Resen drugi zadatak sa regexom

---
+++ 02_EncryptingPassword/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _02_EncryptingPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"(.+)[>](?<digits>[0-9]{3})[|](?<lowLetters>[a-z]{3})[|](?<upperLetters>[A-Z]{3})[|](?<signs>[^<>]{3})[<](\1)";
+
+            int count = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string passwordAttempt = Console.ReadLine();
+
+                Match isPasswordValid = Regex.Match(passwordAttempt, regex);
+
+                StringBuilder output = new StringBuilder();
+
+                if (isPasswordValid.Success)
+                {
+                    output.Append("Password: ");
+                    output.Append(isPasswordValid.Groups["digits"].Value);
+                    output.Append(isPasswordValid.Groups["lowLetters"].Value);
+                    output.Append(isPasswordValid.Groups["upperLetters"].Value);
+                    output.Append(isPasswordValid.Groups["signs"].Value);
+                }
+                else
+                {
+                    output.Append("Try another password!");
+                }
+
+                Console.WriteLine(output.ToString());
+
+            }
+
+
+        }
+    }
+}
---
+++ 02_EncryptingPassword/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("02_EncryptingPassword")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("02_EncryptingPassword")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("433a2e72-0c09-41ca-b522-b2a0dee0da64")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
